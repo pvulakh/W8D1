b0dcaad1c9943ed2101a2a9420e2a08d07812842
App Academy Student <aa@Apps-Mac-mini-3.local>
2019-03-25 18:09:25 -0400

    Wrap up benchbnb day 2

---
+++ benchbnb/frontend/actions/bench_actions.js
@@ -0,0 +1,15 @@
+import * as BenchApiUtil from '../util/bench_api_util';
+
+export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
+
+export const receiveBenches = benches => {
+    return {
+        type: RECEIVE_BENCHES,
+        benches
+    };
+};
+
+export const fetchBenches = () => dispatch => {
+    return BenchApiUtil.fetchBenches().then(benches => dispatch(receiveBenches(benches)));
+};
+
